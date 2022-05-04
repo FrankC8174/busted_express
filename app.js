87f2ba95563bb36f3c78aeee68bc4090044499d6
Franklin Simpson <simpson.franklin44@gmail.com>
2022-05-04 14:00:37 -0400

    Corrections

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3333;
 
-const express = require('express'),
-    es6Renderer = require('express-es6-template-engine');
+const express = require('express')
+    const es6Renderer = require('express-es6-template-engine');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.set('view engine', 'html');
 
 const server = http.createServer(app);
 
-server.listen(PORT, HOSTNAME, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 });
 
